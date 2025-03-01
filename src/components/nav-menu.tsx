ffded41e52b96abada231cd5f082e9fd23969c62
pug-bugger <wrongpugbug@gmail.com>
2025-03-01 15:51:02 +0200

    init

---
+++ src/components/nav-menu.tsx
@@ -0,0 +1,69 @@
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+} from "@/components/ui/navigation-menu";
+import { NavigationMenuProps } from "@radix-ui/react-navigation-menu";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import clsx from "clsx";
+
+export const NavMenu = (props: NavigationMenuProps) => {
+  const pathname = usePathname(); // Получаем текущий путь
+
+  const menuItems = [
+    { label: "Home", href: "/" },
+    { label: "Blog", href: "/blog" },
+    { label: "About", href: "/about" },
+    { label: "Contact Us", href: "/contact" },
+  ];
+
+  return (
+    <NavigationMenu {...props}>
+      <NavigationMenuList className="gap-6 space-x-0 data-[orientation=vertical]:flex-col data-[orientation=vertical]:items-start">
+        {menuItems.map(({ label, href }) => (
+          <NavigationMenuItem key={href}>
+            <NavigationMenuLink asChild>
+              <Link
+                href={href}
+                className={clsx(
+                  "items-center justify-center gap-2 whitespace-nowrap text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 border border-input bg-background hover:bg-accent h-10 px-4 py-2 hidden sm:inline-flex rounded-full",
+                  pathname === href
+                    ? "bg-primary text-primary-foreground hover:bg-primary/90 hover:text-primary-foreground"
+                    : ""
+                )}
+              >
+                {label}
+              </Link>
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+        ))}
+      </NavigationMenuList>
+    </NavigationMenu>
+    // <NavigationMenu {...props}>
+    //   <NavigationMenuList className="gap-6 space-x-0 data-[orientation=vertical]:flex-col data-[orientation=vertical]:items-start">
+    //     <NavigationMenuItem>
+    //       <NavigationMenuLink asChild>
+    //         <Link href="#">Home</Link>
+    //       </NavigationMenuLink>
+    //     </NavigationMenuItem>
+    //     <NavigationMenuItem>
+    //       <NavigationMenuLink asChild>
+    //         <Link href="#">Blog</Link>
+    //       </NavigationMenuLink>
+    //     </NavigationMenuItem>
+    //     <NavigationMenuItem>
+    //       <NavigationMenuLink asChild>
+    //         <Link href="#">About</Link>
+    //       </NavigationMenuLink>
+    //     </NavigationMenuItem>
+    //     <NavigationMenuItem>
+    //       <NavigationMenuLink asChild>
+    //         <Link href="#">Contact Us</Link>
+    //       </NavigationMenuLink>
+    //     </NavigationMenuItem>
+    //   </NavigationMenuList>
+    // </NavigationMenu>
+  );
+};
---
+++ src/pages/init-page.tsx
@@ -0,0 +1,20 @@
+import { Button } from "@/components/ui/button";
+
+export default function InitPage() {
+  return (
+    <>
+      <section className="fixed top-24 inset-x-4 h-16 py-24 flex flex-col items-center text-center gap-8">
+        <h1 className="text-4xl font-bold">_Ordearly is awesome</h1>
+        <p className="text-2xl text-muted-foreground">
+          App to help you manage your orders and customers aaand suppliers
+          easily!
+        </p>
+      </section>
+
+      <div className="relative top-64 inset-x-4 h-16 py-24  flex gap-6 items-center justify-center">
+        <Button variant="secondary">Learn More</Button>
+        <Button>Get Started</Button>
+      </div>
+    </>
+  );
+}
